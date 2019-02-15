f5a4fb47b46dba4ca47c94105ce3100bcd1c3155
Shinho Kang <shinho.kang@honestly.com>
2019-02-15 17:26:12 +0100

    different implementation of component

--- typescript-vue-tutorial/src/index.ts
+++ typescript-vue-tutorial/src/index.ts
@@ -1,17 +1,38 @@
 import Vue from "vue";
-import HelloComponent from "./components/Hello.vue";
+import HelloComponent1 from "./components/Hello.vue";
+import HelloComponent2 from "./components/Hello2.vue";
+import HelloComponent3 from "./components/Hello3.vue";
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
+    <h1>Typical ts</h1>
+    <div>
         Name: <input v-model="name" type="text">
-        <hello-component :name="name" :initialEnthusiasm="5" />
+        <hello-component1 :name="name" :initialEnthusiasm="5" />
+    </div>
+    <br/>
+    <br/>
+    <h1>Class component</h1>
+    <div>
+        Name: <input v-model="name" type="text">
+        <hello-component2 :name="name" :initialEnthusiasm="5" />
+    </div>
+    <br/>
+    <br/>
+    <h1>Object component</h1>
+    <div>
+        Name: <input v-model="name" type="text">
+        <hello-component3 :propExample="{ name:name, initialEnthusiasm: 5 }" />
+    </div>
     </div>
     `,
     data: { name: "World" },
     components: {
-        HelloComponent
+        HelloComponent1,
+        HelloComponent2,
+        HelloComponent3,
     }
 });
 
