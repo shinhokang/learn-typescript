1495aeceb40d000ec0c0e2143f3b960d55ecf620
Shinho Kang <shinho.kang@honestly.com>
2019-02-15 17:40:01 +0100

    add vue proptype way

--- typescript-vue-tutorial/src/index.ts
+++ typescript-vue-tutorial/src/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import HelloComponent1 from "./components/Hello.vue";
 import HelloComponent2 from "./components/Hello2.vue";
 import HelloComponent3 from "./components/Hello3.vue";
+import HelloComponent4 from "./components/Hello4.vue";
 
 let v = new Vue({
     el: "#app",
@@ -12,19 +13,20 @@ let v = new Vue({
         Name: <input v-model="name" type="text">
         <hello-component1 :name="name" :initialEnthusiasm="5" />
     </div>
-    <br/>
-    <br/>
     <h1>Class component</h1>
     <div>
         Name: <input v-model="name" type="text">
         <hello-component2 :name="name" :initialEnthusiasm="5" />
     </div>
-    <br/>
-    <br/>
     <h1>Object component</h1>
     <div>
         Name: <input v-model="name" type="text">
         <hello-component3 :propExample="{ name:name, initialEnthusiasm: 5 }" />
+    </div>
+    <h1>Vue's prop type</h1>
+    <div>
+        Name: <input v-model="name" type="text">
+        <hello-component4 :name="name" :initialEnthusiasm="5" />
     </div>
     </div>
     `,
@@ -33,6 +35,7 @@ let v = new Vue({
         HelloComponent1,
         HelloComponent2,
         HelloComponent3,
+        HelloComponent4,
     }
 });
 
